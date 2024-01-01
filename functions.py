108ff8c60ea7f40a37ce9c516bd4a2d14488810a
syk-rahmadi <98292604+syk-rahmadi@users.noreply.github.com>
2024-01-01 22:39:28 +0800

    Day 6 part of 100 days python learning

---
+++ functions.py
@@ -0,0 +1,17 @@
+print("Hello World!")
+print(len("Hello World!"))
+
+#Built own functions with keyword "def"
+def my_function():
+    print("Hello World!")
+    print("Happy New Years 2024!")
+
+#To trigger function is by "calling" the function
+
+my_function()
+
+#def my_function()
+    #Do this (give ident with 4 spaces)
+    #Then do this
+    #Finally do this
+
